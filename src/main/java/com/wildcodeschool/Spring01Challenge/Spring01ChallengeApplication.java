6d5eb961d3a3051b104a430a09425d1a760b21fa
Vianney Accart <vianneyaccart@gmail.com>
2021-12-19 19:29:26 +0100

    Create DoctorController and routing for each doctors

---
+++ src/main/java/com/wildcodeschool/Spring01Challenge/Spring01ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.Spring01Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring01ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring01ChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/Spring01Challenge/Spring01ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.Spring01Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring01ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
